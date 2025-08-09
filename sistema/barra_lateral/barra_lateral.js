462ba2340710c58a45f8852c6ffc9e9d3dd26d04
SuporteNSBaseTech <sup.nsbasetech@gmail.com>
2025-08-09 02:12:24 +0000

    barra lateral unica OK

---
+++ sistema/barra_lateral/barra_lateral.js
@@ -0,0 +1,39 @@
+verificaAutenticado()
+
+
+document.getElementById("ch-side").addEventListener("change", event => {
+  const mainSide = document.getElementById("main-side")
+  if (event.target.checked) {
+    mainSide.classList.remove("off")
+  }
+  else {
+    mainSide.classList.add("off")
+  }
+})
+
+
+
+let Nome = '';
+let Usuario = ''
+
+
+  ; (async () => {
+    const token = localStorage.getItem(CHAVE)
+
+    const response = await fetch('/verify', {
+      body: JSON.stringify({ token }),
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+
+    const data = await response.json()
+    Nome = data.Nome;
+    Usuario = data.Usuario;
+
+    const userGreeting = document.getElementById('userGreeting');
+    userGreeting.textContent = `Olá, ${Nome}!`;
+
+   })().catch(console.error)
